aaa09498a0d4ddec39603b75497970ffe35fc70f
JuniorBMS <67273128+JuniorBMS@users.noreply.github.com>
2021-04-26 22:49:51 -0300

    TETANDO SUBIR OS VIDEOS NO AZURE

--- js/paginas.js
+++ js/paginas.js
@@ -33,9 +33,27 @@ function contato(){
 
 $(function(){
     $('.carrossel-videos').slick({
-        infinite: true,
+        /* infinite: false,
         slidesToShow: 3,
-        slidesToScroll: 3
-    });
-     
+        slidesToScroll: 3 */
+        infinite: false,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                infinite: true
+            }
+            }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                dots: true
+            }
+            }, {
+            breakpoint: 300,
+            settings: "unslick" // destroys slick
+
+            }
+        ]
+    });  
 });
