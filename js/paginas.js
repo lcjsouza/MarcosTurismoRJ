99709a998ce207f386589aca2429cc68f76c28c8
JuniorBMS <67273128+JuniorBMS@users.noreply.github.com>
2021-05-03 22:43:17 -0300

    TROCA DAS FOTOS DO CARROSSEL INICIAL, ACERTO NO MODAL DE CONTATO, OUTROS ACERTOS NO CSS.

--- js/paginas.js
+++ js/paginas.js
@@ -5,19 +5,7 @@ function sobre(){
     $(".painel-contato").hide();
     $(".nav-inicio").removeClass("active");
     $(".nav-sobre").addClass("active");
-    $(".nav-galeria").removeClass("active");
-    $(".nav-contato").removeClass("active");   
-}
-
-function contato(){
-    $(".painel-inicio").hide();
-    $(".painel-sobre").hide();
-    $(".painel-galeria").hide();
-    $(".painel-contato").show();
-    $(".nav-inicio").removeClass("active");
-    $(".nav-sobre").removeClass("active");
-    $(".nav-galeria").removeClass("active");
-    $(".nav-contato").addClass("active");   
+    $(".nav-galeria").removeClass("active"); 
 }
 
 $(function(){
